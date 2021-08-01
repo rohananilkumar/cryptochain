74edd34bd7cb50c3bacb32b5118ea7af83aca0c1
ROHAN <ro.anilpnr@gmail.com>
2021-08-01 15:55:24 +0530

    Setting up PubSub and cross-dev(for random port assignment)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const Blockchain = require('./blockchain');
+const PubSub = require('./pubsub');
 const app = express();
 app.use(express.json());
 
 const blockchain = new Blockchain();
+const pubsub = new PubSub({blockchain});
+setTimeout(()=>pubsub.broadcastChain(),1000)
 
 
 app.get('/api/blocks', (req, res)=>{
@@ -16,7 +19,13 @@ app.post('/api/mine', (req, res)=>{
     res.redirect('/api/blocks');
 });
 
-const PORT = 3000;
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+if(process.env.GENERATE_PEER_PORT === 'true'){
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random()*1000);   //setting random number (3001-4000)
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, ()=>{
     console.log(`Running on ${PORT}...`)
 })
--- pubsub.js
+++ pubsub.js
@@ -1,24 +1,50 @@
 const redis = require('redis');
 
 const CHANNELS = {
-    TEST:'TEST'
+    TEST:'TEST',
+    BLOCKCHAIN:'BLOCKCHAIN'
 }
 
 class PubSub{
-    constructor(){
+    constructor({blockchain}){
+        this.blockchain = blockchain;
+
         this.publisher = redis.createClient();  //Able to publish when needed
         this.subscriber = redis.createClient(); //Able to listen when needed
 
-        this.subscriber.subscribe(CHANNELS.TEST);   //Subscribes to the channel that it's on
+        this.subscribeToChannels();
 
         this.subscriber.on('message', (channel, message)=>this.handleMessage(channel, message));    //Setting callback to the 'message' event
     }
 
     handleMessage(channel, message){
         console.log(`Message recieved. Channel:${channel}. Message:${message}`);
+
+        const parsedMessage = JSON.parse(message);
+
+        if(channel === CHANNELS.BLOCKCHAIN){
+            this.blockchain.replaceChain(parsedMessage);    //Replace blockchain when new chain is received
+        }
+    }
+
+    subscribeToChannels(){
+        Object.values(CHANNELS).forEach(channel=>{
+            this.subscriber.subscribe(channel);
+        })
+    }
+
+    publish({channel, message}){
+        this.publisher.publish(channel, message);
+    }
+
+    broadcastChain(){
+        console.log(this.blockchain);
+        this.publish({
+            channel:CHANNELS.BLOCKCHAIN,
+            message:JSON.stringify(this.blockchain.chain)   //Boradcast chain 
+        })
     }
 
 }
 
-const testPubSub = new PubSub();
-setTimeout(()=>testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
+module.exports = PubSub;
