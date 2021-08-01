100a5586dc3a1b6870be0680f377ee1862a6beef
ROHAN <ro.anilpnr@gmail.com>
2021-08-01 16:17:02 +0530

    SyncChain on startup

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
+const request = require('request');
 const app = express();
 app.use(express.json());
 
 const blockchain = new Blockchain();
 const pubsub = new PubSub({blockchain});
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 
 app.get('/api/blocks', (req, res)=>{
@@ -19,7 +23,16 @@ app.post('/api/mine', (req, res)=>{
     res.redirect('/api/blocks');
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = () =>{
+    request({url:`${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response,body)=>{
+        if(!error && response.statusCode === 200){
+            const rootChain = JSON.parse(body);
+            console.log('replace chain on a sync with', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    })
+};
+
 let PEER_PORT;
 if(process.env.GENERATE_PEER_PORT === 'true'){
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random()*1000);   //setting random number (3001-4000)
@@ -27,5 +40,6 @@ if(process.env.GENERATE_PEER_PORT === 'true'){
 
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, ()=>{
-    console.log(`Running on ${PORT}...`)
-})
+    console.log(`Running on ${PORT}...`);
+    syncChains();   //Here
+})  
