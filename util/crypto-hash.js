29ea504183d3be5c6abb43807f3c9b520a25f00b
ROHAN <ro.anilpnr@gmail.com>
2021-08-03 14:21:03 +0530

    update cryptohash and add transaction update function

--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -2,7 +2,7 @@ const crypto = require('crypto'); //native crypto package for nodejs
 
 const cryptoHash = (...args) => {
     const hash = crypto.createHash('sha256');
-    hash.update(args.sort().join(' ')); //Sets the input for the hash
+    hash.update(args.map((input)=>JSON.stringify(input)).sort().join(' ')); //Sets the input for the hash
     return hash.digest('hex');  //Returns the result of the hash
 };
 
