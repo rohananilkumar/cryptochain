00815151396b42ec9b1f1b11e0eda4b486f358ee
ROHAN <ro.anilpnr@gmail.com>
2021-08-07 14:21:23 +0530

    parcel setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-render(<div>Cryptochain in React!</div>, document.getElementById('root'));
+render(<div>Cryptochain in React! Yay</div>, document.getElementById('root'));
